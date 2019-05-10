e3b8136df7707826b5ea0ae17cdc89750a7a9973
ppdraga <ppdraga@mail.ru>
2019-05-10 19:16:49 +0300

    fix errors

--- lesson-1/task-7.py
+++ lesson-1/task-7.py
@@ -7,7 +7,7 @@ a = int(input('Введите сторону а '))
 b = int(input('Введите сторону b '))
 c = int(input('Введите сторону c '))
 
-if a + b > c:
+if a + b > c and a + c > b and b + c > a:
     if (a==b or a==c or b==c):
         if a==b and b==c:
             print('Это равносторонний треугольник')
